48197b803535f16fbe87c2d61349ef6efe02f020
Guoguo Yao <guoguo.yao@univ.ox.ac.uk>
2024-10-20 05:52:25 +0800

    Test Controller

---
+++ uuv_mission/control.py
@@ -0,0 +1,16 @@
+class PDController:
+    def __init__(self, KP: float = 0.15, KD: float = 0.5):
+        self.KP = KP
+        self.KD = KD
+        self.previous_error = 0.0
+
+    def control(self, reference: float, output: float) -> float:
+        error = reference - output
+        derivative = error - self.previous_error
+        control_action = self.KP * error + self.KD * derivative
+        self.previous_error = error
+        return control_action
+
+# Example usage:
+# pd_controller = PDController()
+# control_action = pd_controller.control(reference_value, output_value)
